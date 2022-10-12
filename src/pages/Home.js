2dbfeda28cb4716a016846b691861899cdedb324
josephBabal <jbabal301@gmail.com>
2022-10-12 12:25:02 -0700

    check fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 export default function Home(props) {
   return (
     <div className="background-img">
-      <Navbar username={props.username} />
+      <Navbar username={props.username}/>
     
       <div id="slide-container">
         <p className="slide-intro">Here at My Journey...</p>
@@ -15,4 +15,4 @@ export default function Home(props) {
     </div>
   )
 }
-
+   
