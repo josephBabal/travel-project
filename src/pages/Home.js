df343450e48870f9d688c0776073497920fe08fa
josephBabal <jbabal301@gmail.com>
2022-10-12 11:07:30 -0700

    deleted comments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 export default function Home(props) {
-  // const addBodyClass = className => document.body
-  // useEffect(() => {
-  //   document.body.classList.add('background-img')
-  // })
-
   return (
     <div className="background-img">
       <Navbar username={props.username} />
-       {/* {useEffect(() => {
-           document.body.classList.contains('login-background') ?
-           document.body.classList.remove('login-background') :
-           document.body.classList.add('background-img')
-        },[])} */}
-      
+    
       <div id="slide-container">
         <p className="slide-intro">Here at My Journey...</p>
         <p className="slide-txt"> You can write reviews of destinations 
